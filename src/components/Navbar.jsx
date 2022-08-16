4850d2c94a6e796e27c8b00ba56d0bf105125305
eudondian <eudondian@gmail.com>
2022-08-16 22:48:56 +0100

    Create Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+        <Link to='/' className='nav-title'>Math Magicians</Link>
+        <ul>
+            <li><Link to='/home'>Home</Link></li>
+            <li><Link to='/calculator'>Calculator</Link></li>
+            <li><Link to='/quote'>Quote</Link></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
